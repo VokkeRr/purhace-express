d64e4ba278c96dd3f000e43aef64cfe41c413528
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-28 10:39:50 +0000

    Deploy VokkeRr/purhace-express to github.com/VokkeRr/purhace-express.git:gh-pages

---
+++ precache-manifest.2a7f92e5dac31d16c6ca2320aeefaf76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342400a7b0f889ce8d26105d2e981e25",
+    "url": "/purhace-express/index.html"
+  },
+  {
+    "revision": "0999dc53bda636011bf8",
+    "url": "/purhace-express/static/css/main.9d7520bb.chunk.css"
+  },
+  {
+    "revision": "3239c4c284ed4063e7b1",
+    "url": "/purhace-express/static/js/2.59a6e9d1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/purhace-express/static/js/2.59a6e9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0999dc53bda636011bf8",
+    "url": "/purhace-express/static/js/main.577918b3.chunk.js"
+  },
+  {
+    "revision": "5c01cf884e2a8c899d43",
+    "url": "/purhace-express/static/js/runtime-main.21bb0f82.js"
+  },
+  {
+    "revision": "29e4cf8bf68808bc68b83992e54a99ff",
+    "url": "/purhace-express/static/media/loader.29e4cf8b.gif"
+  }
+]);
